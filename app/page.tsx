f8f0fd07ebfd030ef607bc382cbc02e974758cd1
serg-kan <serg.kan096@gmail.com>
2023-11-23 22:57:08 +0300

    added links

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         className="flex flex-col md:flex-row items-center space-y-4 md:space-x-10 lg:space-x-20 justify-center bg-repeat bg-pattern m-5 h-4/5" 
         style={{ backgroundImage: "url('/images/bg-pattern.jpg')"}}
       >
-        <div className="relative rounded-full border-white border-8 overflow-hidden w-full h-full max-h-[300px] max-w-[300px]">
+        <div className="relative rounded-full border-white border-8 overflow-hidden w-full h-full max-h-[300px] max-w-[300px] min-h-[100px]">
           <Image
                 src="/images/photo.jpg"
                 fill
@@ -28,21 +28,24 @@ export default function Home() {
           </h1>
 
           <div className="leading-7 lg:text-xl">
-            <Typewriter
-              options={{
-                strings: ['Welcome to my blog'],
-                autoStart: true,
-                loop: true,
-              }}
-            />
+            <p>Product Manager, ex-web dev, dad</p>
           </div>
           
 
           <div className="flex justify-center md:justify-start space-x-4">
-            <FaLinkedin size={30}/>
-            <FaTelegram size={30} />
-            <FaGithub size={30} />
-            <FaYoutube size={30} />
+            <a href="https://www.linkedin.com/in/sergkan/" target="_blank" rel="noreferrer">
+              <FaLinkedin size={30} />
+            </a>
+            <a href="https://t.me/kan_notes" target="_blank" rel="noreferrer">         
+              <FaTelegram size={30} />
+            </a>
+            <a href="https://github.com/serg-kan/" target="_blank" rel="noreferrer">         
+              <FaGithub size={30} />
+            </a>
+            <a href="https://www.youtube.com/@serg_kan" target="_blank" rel="noreferrer">
+              <FaYoutube size={30} />
+            </a>
+            
           </div>
 
         </div>
